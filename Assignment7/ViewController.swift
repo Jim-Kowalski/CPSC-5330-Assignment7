33e2470de70af3a8127eea3c7b2e5814e823c5e3
James Kowalski <jzk0156@auburn.edu>
2024-02-22 17:06:37 -0600

    Initial Commit

---
+++ Assignment7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment7
+//
+//  Created by James Kowalski on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
